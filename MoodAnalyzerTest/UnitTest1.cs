83579c321c48c1edd120b95f97efa2568518321f
Poojachakor <chakorpooja07@gmail.com>
2023-06-23 17:27:16 +0530

    Add: MoodAnalyzer

---
+++ MoodAnalyzerTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+using MoodAnalyzer;
+
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly MoodAnalyzer.Mood mood;
+
+        public UnitTest1()
+        {
+            mood = new MoodAnalyzer.Mood();
+        }
+        //TestCase1.1 --- Return Sad
+        [TestMethod]
+        public void Given_Mood_Sad_Should_Return_Sad()
+        {
+            //Arrange
+            string expectectResult = "Sad";
+
+            //Act
+            string result = mood.AnalyzeMood("I am in sad Mood");
+
+            //Assert
+            Assert.AreEqual(expectectResult, result);
+
+        }
+
+        [TestMethod]
+        public void Given_Mood_Happy_Should_Return_Happy()
+        {
+            string expectedresult = "Happy";
+
+            string result = mood.AnalyzeMood("I am in Happy Mood");
+
+            Assert.AreEqual(expectedresult, result);
+        }
+    }
+}
